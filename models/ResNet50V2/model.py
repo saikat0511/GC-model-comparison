1fb972468f285869905401a445f7d1dcf4d56602
saikat0511 <saikat0511@gmail.com>
2023-05-11 01:45:14 +0530

    initial commit

---
+++ models/ResNet50V2/model.py
@@ -0,0 +1,23 @@
+def get_model(num_class):
+    import tensorflow as tf
+    from keras.applications.resnet_v2 import ResNet50V2, preprocess_input
+    IMG_SHAPE = (224, 224, 3)
+
+    base_model = ResNet50V2(
+        input_shape = IMG_SHAPE,
+        include_top = False,
+        weights     = 'imagenet')
+    
+    base_model.trainable = False
+
+    ip_layer = tf.keras.Input(shape=IMG_SHAPE)
+    x        = preprocess_input(ip_layer)
+    x        = base_model(x, training=False)
+    x        = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x        = tf.keras.layers.Dense(512, activation='relu')(x)
+    x        = tf.keras.layers.Dropout(.2)(x)
+    op_layer = tf.keras.layers.Dense(num_class, activation='softmax')(x)
+
+    model = tf.keras.Model(inputs=ip_layer, outputs=op_layer)
+    
+    return model
